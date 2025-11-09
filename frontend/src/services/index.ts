3f44f97455c46d020796895adc0928f9fe8881d7
mms-11 <140762703+mms-11@users.noreply.github.com>
2025-11-09 14:54:07 -0300

    feat: adicionar arquivo de serviços com exportações para API, autenticação, alunos, classes, avaliações e dashboard

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,9 @@
+export { default as api } from './api';
+export { authService } from './auth.service';
+export { studentService } from './student.service';
+export { classService } from './class.service';
+export { assessmentService } from './assessment.service';
+export { dashboardService } from './dashboard.service';
+export * from '../types/class.types';
+export * from '../types/assessment.types';
+export * from './class.service'; 
